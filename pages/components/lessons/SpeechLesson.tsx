5eda852b666d399fefaef01dc879f240a1c58f13
Rahul Shah <rshah98626@gmail.com>
2022-12-10 15:34:32 -0600

    about 3.5 hours

---
+++ pages/components/lessons/SpeechLesson.tsx
@@ -0,0 +1,60 @@
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition"
+import { useSpeechSynthesis } from 'react-speech-kit'
+import { Button } from "react-bootstrap";
+
+export default function SpeechLesson() {
+  const { speak, voices, supported, speaking } = useSpeechSynthesis();
+  const voiceIndex = voices.findIndex((voice: SpeechSynthesisVoice) => voice.lang === 'es-ES')
+  // ranges from 0.5 to 2
+  const dictationSpeed = 0.75
+
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+    isMicrophoneAvailable
+  } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <div>Your browser does not support speech recognition. Please switch browsers.</div>
+  }
+
+  if (!supported) {
+    return <div>Your browser does not support speech dictation. Please switch browsers.</div>
+  }
+
+  if (!isMicrophoneAvailable) {
+    return <div>Enable access to your microphone to complete this lesson.</div>
+  }
+
+  return (
+    <div>
+      <div>
+        <Button
+          variant="primary"
+          onClick={() => speak({ text: '¿Qué estás comiendo?', voice: voices[voiceIndex], rate: dictationSpeed })}
+        >
+          Click to speak
+        </Button>
+        <Button 
+          variant="primary" 
+          onClick={() => SpeechRecognition.startListening({ language: 'es-US' })}
+        >
+          Start listening
+        </Button>
+      </div>
+      <div>
+        <Button 
+          variant="danger"
+          onClick={() => SpeechRecognition.stopListening()}
+        >
+          Stop listening
+        </Button>
+      </div>
+      <div>
+        <h6>This is the transcript: {transcript}</h6>
+      </div>
+    </div>
+  )
+}
--- pages/components/lessons/VideoPronunciationLesson.tsx
+++ pages/components/lessons/VideoPronunciationLesson.tsx
@@ -10,8 +10,12 @@ export default function VideoPronunciationLesson(props: {
           <source src="/videos/whatAreYouEating.mp4" type="video/mp4" />
         </video>
       </Ratio>
-      <h4>¿Qué estás comiendo?</h4>
-      <h4>What are you eating?</h4>
+      <div style={{ display: 'flex', justifyContent: 'center', margin: "1em" }}>
+        <h4>¿Qué <span style={{ color: '#0083ff' }}>estás</span> comiendo?</h4>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center', margin: "1em" }}>
+        <h4>What are you eating?</h4>
+      </div>
       <Button variant='primary' onClick={props.incrementIndex}>Next</Button>
     </div>
   )
