75228f61ab42a2e1782a3df6f2d202990ca5e24d
Rahul Shah <rshah98626@gmail.com>
2022-12-13 16:58:45 -0600

    comment

--- pages/components/misc/SpeakingComponent.tsx
+++ pages/components/misc/SpeakingComponent.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useTts } from 'tts-react'
 import type { TTSHookProps } from 'tts-react'
 import { useCallback, useEffect, useState } from "react";
