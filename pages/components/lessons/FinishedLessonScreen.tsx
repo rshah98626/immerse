6f61f5ff798c47988c4341fd7f2daeaaf4fd90cb
Rahul Shah <rshah98626@gmail.com>
2022-12-15 17:29:54 -0600

    feat: adding support for confetti in end lesson screen (#14)
    * feat: adding support for confetti in end lesson screen
    
    * refactoring

--- pages/components/lessons/FinishedLessonScreen.tsx
+++ pages/components/lessons/FinishedLessonScreen.tsx
@@ -1,12 +1,29 @@
-import ConfettiExplosion from 'react-confetti-explosion';
+import Confetti from "react-confetti";
+import { useState, useRef, useEffect } from "react";
+import { Container } from "react-bootstrap";
 
 export default function FinishedLessonScreen() {
+  const [height, setHeight] = useState(0);
+  const [width, setWidth] = useState(0);
+  const [showConfetti, setShowConfetti] = useState(true);
+  const confetiRef: React.Ref<HTMLInputElement> = useRef(null);
+
+  useEffect(() => {
+    if (confetiRef.current !== null) {
+      setHeight(confetiRef.current.clientHeight);
+      setWidth(confetiRef.current.clientWidth);
+    }
+  }, []);
+
   return (
-    <div style={{height: 500, width: 500}}>
-      <h1>Done!</h1>
-      <ConfettiExplosion 
-        duration={1000000}
-      />
-    </div>
+    <Container style={{ minHeight: '50vh'}} ref={confetiRef} className={"d-flex flex-column justify-content-center align-items-center"}>
+      {showConfetti && 
+        <Confetti 
+          numberOfPieces={500} width={width} height={height} onConfettiComplete={() => setShowConfetti(false)}
+          recycle={false}
+        />
+      }
+      <h1>Done with lesson!</h1>
+    </Container>
   )
 }
