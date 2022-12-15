17b24cb0af1904a97a3f7475d5a0e38b0d27c723
Rahul Shah <rshah98626@gmail.com>
2022-12-15 17:24:19 -0600

    feat: adding support for confetti in end lesson screen

--- pages/components/lessons/FinishedLessonScreen.tsx
+++ pages/components/lessons/FinishedLessonScreen.tsx
@@ -1,12 +1,38 @@
-import ConfettiExplosion from 'react-confetti-explosion';
+import Confetti from "react-confetti";
+import { StyleSheet, css } from 'aphrodite'
+import { useState, useRef, useEffect } from "react";
+import { Container } from "react-bootstrap";
+
+const styles = StyleSheet.create({
+  containerStyles: {
+    // minHeight: '50vh',
+  }
+})
 
 export default function FinishedLessonScreen() {
+  const [height, setHeight] = useState(0);
+  const [width, setWidth] = useState(0);
+  const [showConfetti, setShowConfetti] = useState(true);
+  const confetiRef: React.Ref<HTMLInputElement> = useRef(null);
+
+  useEffect(() => {
+    if (confetiRef.current !== null) {
+      setHeight(confetiRef.current.clientHeight);
+      setWidth(confetiRef.current.clientWidth);
+    }
+  }, []);
+
   return (
-    <div style={{height: 500, width: 500}}>
-      <h1>Done!</h1>
-      <ConfettiExplosion 
-        duration={1000000}
-      />
-    </div>
+    <Container style={{ minHeight: '50vh'}} ref={confetiRef} className={"d-flex flex-column justify-content-center align-items-center" + ' ' + css(styles.containerStyles)}>
+      {/* <div className={css(styles.divInTheMiddle)}> */}
+        {showConfetti && 
+          <Confetti 
+            numberOfPieces={500} width={width} height={height} onConfettiComplete={() => setShowConfetti(false)}
+            recycle={false}
+          />
+        }
+        <h1>Done with lesson!</h1>
+      {/* </div> */}
+    </Container>
   )
 }
